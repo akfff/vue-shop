db6bc213e92b500c00f7faaeea2bc028037668ba
akfff <junkchen@vip.qq.com2385382029@qq.com>
2022-10-13 19:12:14 +0800

    login

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,16 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+// 按需导入
+import { Form, FormItem } from "element-ui";
+import { Input } from "element-ui";
+// 导入弹框组件
+import { Message } from "element-ui";
+
 
 Vue.use(Button)
+// 注册全局组件
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 全局挂载
+Vue.prototype.$message = Message
